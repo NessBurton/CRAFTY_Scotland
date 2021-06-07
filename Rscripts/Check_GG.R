56ab7f582a4470e2d10261fd62aa683ea1ca1859
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-06-07 17:19:51 +0100

    check GG run with thesis params

---
+++ Rscripts/Check_GG.R
@@ -0,0 +1,257 @@
+
+library(vroom)
+library(tidyverse)
+library(ggplot2)
+library(reshape2)
+library(ggplot2)
+library(spatstat)
+library(wesanderson)
+
+### directories ----------------------------------------------------------------
+
+#wd <- "D:/"
+wd <- "~/eclipse-workspace/CRAFTY_Scotland/"
+
+dirResults <- paste0(wd,"output/Thesis/")
+#dirMetrics <- paste0(wd,"CRAFTY_Scotland/vision_metrics")
+
+dfVision <-
+  list.files(path = paste0(dirResults,"Green_Gold/"),
+             pattern = "*.csv", 
+             full.names = T) %>% 
+  grep("-Cell-", value=TRUE, .) %>% 
+  #map_df(~read_csv(., col_types = cols(.default = "c")))
+  map_df(~read.csv(.))
+
+# region
+dfVision$Capital.region[which(dfVision$Capital.region==0)]<-NA
+dfVision$Capital.region[which(dfVision$Capital.region==1)]<-"South Scotland"
+dfVision$Capital.region[which(dfVision$Capital.region==2)]<-"Central"
+dfVision$Capital.region[which(dfVision$Capital.region==3)]<-"Perth & Argyll"
+dfVision$Capital.region[which(dfVision$Capital.region==4)]<-"Grampian"
+dfVision$Capital.region[which(dfVision$Capital.region==5)]<-"Highlands"
+dfVision$Capital.region[which(dfVision$Capital.region==6)]<-"Islands"
+dfVision$Capital.region <- factor(dfVision$Capital.region)
+
+# reclassify agent types to produce simplified land use categories
+dfVision$reclass <- NA
+dfVision$reclass[which(dfVision$Agent=="prodnconifer"|
+                         dfVision$Agent=="prodnbroad"|
+                         dfVision$Agent=="consvnative"|
+                         dfVision$Agent=="multinc"|
+                         dfVision$Agent=="multinb")] <-'native.woodland'
+dfVision$reclass[which(dfVision$Agent=="prodnnconifer"|
+                         dfVision$Agent=="prodnnbroad"|
+                         dfVision$Agent=="multinnc"|
+                         dfVision$Agent=="multinnb")] <- "non.native.woodland"
+dfVision$reclass[which(dfVision$Agent=="multimixed")] <- "mixed.woodland"
+dfVision$reclass[which(dfVision$Agent=="intarable"|
+                         dfVision$Agent=="intpastoral")] <- "intensive.agriculture"
+dfVision$reclass[which(dfVision$Agent=="extarable"|
+                         dfVision$Agent=="extpastoral"|
+                         dfVision$Agent=="agroforestry")] <- "extensive.agriculture"
+dfVision$reclass[which(dfVision$Agent=="estatemulti")] <- 'mixed.estate'
+dfVision$reclass[which(dfVision$Agent=="estatesport")] <- "traditional.sporting.management"
+dfVision$reclass[which(dfVision$Agent=="estateconsv")] <- "conservation.management"
+dfVision$reclass[which(dfVision$Agent=="marginal")] <- "marginal"
+dfVision$reclass[which(dfVision$Agent=="waterurban")] <- "urban.&.waterbodies"
+
+# create simplified all.woodland type for connectivity metric
+dfVision$all.woodland <- NA
+dfVision$all.woodland[which(dfVision$Agent=="prodnconifer"|
+                              dfVision$Agent=="prodnbroad"|
+                              dfVision$Agent=="consvnative"|
+                              dfVision$Agent=="multinc"|
+                              dfVision$Agent=="multinb"|
+                              dfVision$Agent=="prodnnconifer"|
+                              dfVision$Agent=="prodnnbroad"|
+                              dfVision$Agent=="multinnc"|
+                              dfVision$Agent=="multinnb"|
+                              dfVision$Agent=="multimixed")] <- "all.woodland"
+
+
+### woodland % cover per yr
+year_list<-c(2015:2100)
+wood.cover<-rep(0,length(year_list))
+estmW<-rep(0,length(year_list))
+estsW<-rep(0,length(year_list))
+estcW<-rep(0,length(year_list))
+agroW<-rep(0,length(year_list))
+
+# loop to count length of woodland agents
+for (i in c(1:86)) {
+  
+  timestep<-filter(dfVision, Tick==year_list[i])
+  wood.cover[i]<- length(timestep$Agent[timestep$Agent == "consvnative"|
+                                          timestep$Agent == "multinb"|
+                                          timestep$Agent == "multimixed"|
+                                          timestep$Agent == "prodnconifer"|
+                                          timestep$Agent == "prodnbroad"|
+                                          timestep$Agent =="prodnnconifer"|
+                                          timestep$Agent == "prodnnbroad"|
+                                          timestep$Agent == "multinc"|
+                                          timestep$Agent == "multinnb"|
+                                          timestep$Agent == "multinnc"])/length(timestep$Agent)*100
+  estmW[i]<-length(timestep$Agent[which(timestep$Agent=='estatemulti'&timestep$Service.softwood.timber>0|timestep$Service.hardwood.timber>0)])/length(timestep$Agent)*100
+  estsW[i]<-length(timestep$Agent[which(timestep$Agent=='estatesport'&timestep$Service.softwood.timber>0|timestep$Service.hardwood.timber>0)])/length(timestep$Agent)*100
+  estcW[i]<-length(timestep$Agent[which(timestep$Agent=='estateconsv'&timestep$Service.softwood.timber>0|timestep$Service.hardwood.timber>0)])/length(timestep$Agent)*100
+  agroW[i]<-length(timestep$Agent[which(timestep$Agent=='agroforestry'&timestep$Service.softwood.timber>0|timestep$Service.hardwood.timber>0)])/length(timestep$Agent)*100
+  
+}
+
+#wood_cover<-wood.cover+estcW+estmW+estsW+agroW
+#wood_cover<-wood_cover # adjust?
+wood_cover <- wood.cover
+wood_cover2 <- wood.cover+estcW+estmW+estsW+agroW
+
+
+### total.ES.diversity
+
+tES.div<-rep(0,80)
+year_id <- year_list
+
+Ss.timber<-rep(0,length(year_id))
+Sh.timber<-rep(0,length(year_id))
+S.bio<-rep(0,length(year_id))
+S.carbon<-rep(0,length(year_id))
+S.flood<-rep(0,length(year_id))
+S.rec<-rep(0,length(year_id))
+S.lstock<-rep(0,length(year_id))
+S.crop<-rep(0,length(year_id))
+S.emp<-rep(0,length(year_id))
+tot<-rep(0,length(year_id))
+
+# loop to sum production for each service
+for (i in c(1:81)) {
+  
+  timestep<-filter(dfVision, Tick==year_list[i])
+  Ss.timber[i]<-sum(timestep$Service.softwood.timber)
+  Sh.timber[i]<-sum(timestep$Service.hardwood.timber)
+  S.bio[i]<-sum(timestep$Service.biodiversity)
+  S.carbon[i]<-sum(timestep$Service.carbon)
+  S.flood[i]<-sum(timestep$Service.flood.regulation)
+  S.rec[i]<-sum(timestep$Service.recreation)
+  S.lstock[i]<-sum(timestep$Service.livestock)
+  S.crop[i]<-sum(timestep$Service.crop.service)
+  S.emp[i]<-sum(timestep$Service.employment)
+  tot[i]<-(Ss.timber[i]+Sh.timber[i]+S.bio[i]+S.carbon[i]+S.flood[i]+S.rec[i]+S.lstock[i]
+           +S.crop[i]+S.emp[i])
+  
+  # metric based on simpson's diversity index
+  tES.div[i]<-1-(((Ss.timber[i]/tot[i])^2)+((Sh.timber[i]/tot[i])^2)+((S.bio[i]/tot[i])^2)
+                 +((S.carbon[i]/tot[i])^2)+((S.flood[i]/tot[i])^2)+((S.rec[i]/tot[i])^2)
+                 +((S.lstock[i]/tot[i])^2)+((S.crop[i]/tot[i])^2)+((S.emp[i]/tot[i])^2))
+}
+
+
+### total.LU.diversity
+
+tLU.div<-rep(0,81)
+
+# empty columns to be populated by loop
+prod.nn.conifer<-rep(0,length(year_id))
+prod.n.conifer<-rep(0,length(year_id))
+prod.nn.broad<-rep(0,length(year_id))
+prod.n.broad<-rep(0,length(year_id))
+consv.native<-rep(0,length(year_id))
+multi.mixed<-rep(0,length(year_id))
+multi.nnc<-rep(0,length(year_id))
+multi.nc<-rep(0,length(year_id))
+multi.nnb<-rep(0,length(year_id))
+multi.nb<-rep(0,length(year_id))
+agroforestry<-rep(0,length(year_id))
+int.pastoral<-rep(0,length(year_id))
+ext.pastoral<-rep(0,length(year_id))
+int.arable<-rep(0,length(year_id))
+ext.arable<-rep(0,length(year_id))
+estate.sport<-rep(0,length(year_id))
+estate.multi<-rep(0,length(year_id))
+estate.consv<-rep(0,length(year_id))
+marginal<-rep(0,length(year_id))
+water.urban<-rep(0,length(year_id))
+tot<-rep(0,length(year_id))
+
+year_list<-c(2020:2100)
+
+# loop through years
+for(i in c(1:81)) {
+  
+  timestep<-filter(dfVision, Tick==year_list[i])
+  prod.nn.conifer[i]<-length(timestep$Agent[timestep$Agent=="prodnnconifer"])
+  prod.n.conifer[i]<-length(timestep$Agent[timestep$Agent=="prodnconifer"])
+  prod.nn.broad[i]<-length(timestep$Agent[timestep$Agent=="prodnnbroad"])
+  prod.n.broad[i]<-length(timestep$Agent[timestep$Agent=="prodnbroad"])
+  consv.native[i]<-length(timestep$Agent[timestep$Agent=="consvnative"])
+  multi.mixed[i]<-length(timestep$Agent[timestep$Agent=="multimixed"])
+  multi.nnc[i]<-length(timestep$Agent[timestep$Agent=="multinnc"])
+  multi.nc[i]<-length(timestep$Agent[timestep$Agent=="multinc"])
+  multi.nnb[i]<-length(timestep$Agent[timestep$Agent=="multinnb"])
+  multi.nb[i]<-length(timestep$Agent[timestep$Agent=="multinb"])
+  agroforestry[i]<-length(timestep$Agent[timestep$Agent=="agroforestry"])
+  int.pastoral[i]<-length(timestep$Agent[timestep$Agent=="intpastoral"])
+  ext.pastoral[i]<-length(timestep$Agent[timestep$Agent=="extpastoral"])
+  int.arable[i]<-length(timestep$Agent[timestep$Agent=="intarable"])
+  ext.arable[i]<-length(timestep$Agent[timestep$Agent=="extarable"])
+  estate.sport[i]<-length(timestep$Agent[timestep$Agent=="estatesport"])
+  estate.multi[i]<-length(timestep$Agent[timestep$Agent=="estatemulti"])
+  estate.consv[i]<-length(timestep$Agent[timestep$Agent=="estateconsv"])
+  marginal[i]<-length(timestep$Agent[timestep$Agent=="marginal"])
+  water.urban[i]<-length(timestep$Agent[timestep$Agent=="waterurban"])
+  tot[i]<-length(timestep$Agent)
+  
+  # Simpon index for diversity of land uses
+  
+  tLU.div[i]<-1-(((prod.nn.conifer[i]/tot[i])^2)+((prod.n.conifer[i]/tot[i])^2)+((prod.nn.broad[i]/tot[i])^2)+
+                   ((prod.n.broad[i]/tot[i])^2)+((consv.native[i]/tot[i])^2)+((multi.mixed[i]/tot[i])^2)+
+                   ((multi.nnc[i]/tot[i])^2)+((multi.nc[i]/tot[i])^2)+((multi.nnb[i]/tot[i])^2)+((multi.nb[i]/tot[i])^2)+
+                   ((agroforestry[i]/tot[i])^2)+((int.pastoral[i]/tot[i])^2)+((ext.pastoral[i]/tot[i])^2)+
+                   ((int.arable[i]/tot[i])^2)+((ext.arable[i]/tot[i])^2)+((estate.sport[i]/tot[i])^2)+
+                   ((estate.multi[i]/tot[i])^2)+((estate.consv[i]/tot[i])^2)+((marginal[i]/tot[i])^2)+((water.urban[i]/tot[i])^2))
+  
+}
+
+
+### agent extents
+
+native.wood.ext<-c()
+consv.ext<-c()
+int.agri.ext<-c()
+int.timb.ext<-c()
+estates.ext<-c()
+ext.agri.ext<-c()
+
+
+for(i in c(1:81)) {
+  timestep<-filter(dfVision, Tick==year_list[i])
+  native.wood.ext[i]<-length(timestep$Agent[timestep$Agent=="consvnative"])
+  consv.ext[i]<-length(timestep$Agent[timestep$Agent=="estateconsv"|
+                                        timestep$Agent=="consvnative"])
+  int.agri.ext[i]<-length(timestep$Agent[timestep$Agent=="intpastoral" | timestep$Agent=="intarable"])
+  int.timb.ext[i]<-length(timestep$Agent[timestep$Agent=="prodnnconifer" | timestep$Agent=="prodnnbroad" 
+                                         | timestep$Agent=="prodnconifer" | timestep$Agent=="prodnbroad"])
+  estates.ext[i]<-length(timestep$Agent[timestep$Agent=="estatemulti"|timestep$Agent=="estatesport"|
+                                          timestep$Agent=="estateconsv"])
+  ext.agri.ext[i]<-length(timestep$Agent[timestep$Agent=="extpastoral"|timestep$Agent=="extarable"])
+}
+
+
+metrics <- cbind(year_id,
+                 wood_cover,
+                 wood_cover2,
+                 tES.div,
+                 tLU.div,
+                 native.wood.ext,
+                 consv.ext,
+                 int.agri.ext,
+                 int.timb.ext,
+                 estates.ext,
+                 ext.agri.ext)
+
+dfMetrics <- as.data.frame(metrics)
+
+ggplot(dfMetrics, aes(year_id,wood_cover, colour=vision))+
+  geom_line()+
+  theme_bw()
+ggplot(dfMetrics, aes(year_id,wood_cover2, colour=vision))+
+  geom_line()+
+  theme_bw()
