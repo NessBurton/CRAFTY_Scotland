081f341ea64e983e7f5059abe07c22b3eca5913b
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-07-07 16:51:50 +0100

    correct column names

--- Rscripts/CRAFTY_Scotland_create_csvs.R
+++ Rscripts/CRAFTY_Scotland_create_csvs.R
@@ -83,6 +83,7 @@ dirOut <- paste0(wd,"data_Scotland")
 
 dfBehaviour <- read.csv(paste0(dirData,"/BehaviourMaster.csv"))
 colnames(dfBehaviour)[1] <- "Agent"
+colnames(dfBehaviour)[4] <- "givingInDistributionSD"
 dfBehaviour <- dfBehaviour %>% mutate(productionCsvFile = paste0(".//production/",Agent,".csv"))
 #dfBehaviour$Agent <- NULL
 dfBehaviour$aftParamId <- 0
