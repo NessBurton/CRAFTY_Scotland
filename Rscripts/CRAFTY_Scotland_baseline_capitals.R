9e3265114186b1b61dfa4d212be6c84b5c090c51
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-08-03 09:00:30 +0100

    new baseline with edited agri caps

--- Rscripts/CRAFTY_Scotland_baseline_capitals.R
+++ Rscripts/CRAFTY_Scotland_baseline_capitals.R
@@ -40,62 +40,66 @@ capitalsRaw$y <- capitalsRaw$y / 1000
 colnames(capitalsRaw)[4]<-'Agent'
 
 # edit crop capital
-lca.props <- read.csv(paste0(dirData,"/intermediate/lca_props.csv"))
-lca.props$X<-NULL
-nrows<-length(lca.props[,1])
-agri.capital<-rep(NA,nrows)
-for (i in c(1:nrows)) { 
-  if (lca.props$LCA1[i]>=0.7) {
-    agri.capital[i]<-9} 
-  if (lca.props$LCA2[i]>=0.7) {
-    agri.capital[i]<-8}
-  if (lca.props$LCA31[i]>=0.7) {
-    agri.capital[i]<-7}
-  if (lca.props$LCA32[i]>=0.7) {
-    agri.capital[i]<-7}
-  if (lca.props$LCA41[i]>=0.7) {
-    agri.capital[i]<-6}
-  if (lca.props$LCA42[i]>=0.7) {
-    agri.capital[i]<-6}
-  if (lca.props$LCA51[i]>=0.7) {
-    agri.capital[i]<-5}
-  if (lca.props$LCA52[i]>=0.7) {
-    agri.capital[i]<-5}
-  if (lca.props$LCA53[i]>=0.7) {
-    agri.capital[i]<-5}
-  if (lca.props$LCA61[i]>=0.7) {
-    agri.capital[i]<-4}
-  if (lca.props$LCA62[i]>=0.7) {
-    agri.capital[i]<-4}
-  if (lca.props$LCA63[i]>=0.7) {
-    agri.capital[i]<-4}
-  if (lca.props$LCA7[i]>=0.7) {
-    agri.capital[i]<-3}
-  if (lca.props$LCA888[i]>=0.7) {
-    agri.capital[i]<-2}
-  if (lca.props$LCA9500[i]>=0.7) {
-    agri.capital[i]<-1}
-  if (lca.props$LCA999[i]>=0.7) {
-    agri.capital[i]<-1}
-}
-lca.props$agri.capital <- agri.capital
-lca.props[is.na(lca.props)] <- 0
-colnames(lca.props)<-c("id",'lca1','lca2', 'lca31', 'lca32', 
-                       'lca41', 'lca42', 'lca51', 'lcs52', 'lca53',
-                       'lca61', 'lca62', 'lca63', 'lca7', 'lca8', 'lca95', 'lca99',
-                       'area', 'agri.capital')
-coords <- read.csv(paste0(dirData,'/output/lcm_iap_coords.csv'))
-crops <- merge.data.frame(coords, lca.props, by = "id", all.x=TRUE)
-crops$agri.capital<-as.numeric(crops$agri.capital)
-# filter so that it only includes classes which support crops
-# these are (according to LCA documentation) - 1,2,3.1,3.2,4.1,4.2 (6-9 in my reclass)
-# https://www.hutton.ac.uk/sites/default/files/files/soils/lca_map_hutton.pdf
-crops$agri.filter <- NA
-crops$agri.filter[which(crops$agri.capital<6)]<-1 # marker for classes unsuitable for crops
-capitalsRaw$crop.productivity <- crops$agri.capital
-capitalsRaw$agri.filter <- crops$agri.filter
+# lca.props <- read.csv(paste0(dirData,"/intermediate/lca_props.csv"))
+# lca.props$X<-NULL
+# nrows<-length(lca.props[,1])
+# agri.capital<-rep(NA,nrows)
+# for (i in c(1:nrows)) { 
+#   if (lca.props$LCA1[i]>=0.7) {
+#     agri.capital[i]<-9} 
+#   if (lca.props$LCA2[i]>=0.7) {
+#     agri.capital[i]<-8}
+#   if (lca.props$LCA31[i]>=0.7) {
+#     agri.capital[i]<-7}
+#   if (lca.props$LCA32[i]>=0.7) {
+#     agri.capital[i]<-7}
+#   if (lca.props$LCA41[i]>=0.7) {
+#     agri.capital[i]<-6}
+#   if (lca.props$LCA42[i]>=0.7) {
+#     agri.capital[i]<-6}
+#   if (lca.props$LCA51[i]>=0.7) {
+#     agri.capital[i]<-5}
+#   if (lca.props$LCA52[i]>=0.7) {
+#     agri.capital[i]<-5}
+#   if (lca.props$LCA53[i]>=0.7) {
+#     agri.capital[i]<-5}
+#   if (lca.props$LCA61[i]>=0.7) {
+#     agri.capital[i]<-4}
+#   if (lca.props$LCA62[i]>=0.7) {
+#     agri.capital[i]<-4}
+#   if (lca.props$LCA63[i]>=0.7) {
+#     agri.capital[i]<-4}
+#   if (lca.props$LCA7[i]>=0.7) {
+#     agri.capital[i]<-3}
+#   if (lca.props$LCA888[i]>=0.7) {
+#     agri.capital[i]<-2}
+#   if (lca.props$LCA9500[i]>=0.7) {
+#     agri.capital[i]<-1}
+#   if (lca.props$LCA999[i]>=0.7) {
+#     agri.capital[i]<-1}
+# }
+# lca.props$agri.capital <- agri.capital
+# lca.props[is.na(lca.props)] <- 0
+# colnames(lca.props)<-c("id",'lca1','lca2', 'lca31', 'lca32', 
+#                        'lca41', 'lca42', 'lca51', 'lcs52', 'lca53',
+#                        'lca61', 'lca62', 'lca63', 'lca7', 'lca8', 'lca95', 'lca99',
+#                        'area', 'agri.capital')
+# coords <- read.csv(paste0(dirData,'/output/lcm_iap_coords.csv'))
+# crops <- merge.data.frame(coords, lca.props, by = "id", all.x=TRUE)
+# crops$agri.capital<-as.numeric(crops$agri.capital)
+# # filter so that it only includes classes which support crops
+# # these are (according to LCA documentation) - 1,2,3.1,3.2,4.1,4.2 (6-9 in my reclass)
+# # https://www.hutton.ac.uk/sites/default/files/files/soils/lca_map_hutton.pdf
+# crops$agri.filter <- NA
+# crops$agri.filter[which(crops$agri.capital<6)]<-1 # marker for classes unsuitable for crops
+# capitalsRaw$crop.productivity <- crops$agri.capital
+# capitalsRaw$agri.filter <- crops$agri.filter
 
-write.csv(capitalsRaw, paste0(dirData,"/output/capitals_raw_Feb21.csv"),row.names = F)
+dfAgriCaps <- read.csv(paste0(dirData,'/output/agri_capitals.csv'))
+capitalsRaw$crop.productivity <- dfAgriCaps$crop.capital
+capitalsRaw$grassland <- dfAgriCaps$grass.capital
+
+write.csv(capitalsRaw, paste0(dirData,"/output/capitals_raw_Aug21.csv"),row.names = F)
 
 ### normalise ------------------------------------------------------------------
 
@@ -108,12 +112,12 @@ summary(capitalsNRM)
 capitalsNRM$X <- NULL
 
 #capitalsNRM[,c(6:28)] <- normalise(capitalsNRM[,c(6:28)])
-capitalsNRM <- data.frame(capitalsNRM[,c(1:5,29)], lapply(capitalsNRM[6:28], normalise))
+capitalsNRM <- data.frame(capitalsNRM[,c(1:5)], lapply(capitalsNRM[6:28], normalise))
 summary(capitalsNRM)
 capitalsNRM[is.na(capitalsNRM)] <- 0
-capitalsNRM$crop.productivity[which(capitalsNRM$agri.filter==1)]<-0 # remove cap where not suitable for crops
+#capitalsNRM$crop.productivity[which(capitalsNRM$agri.filter==1)]<-0 # remove cap where not suitable for crops
 #remove filter column
-capitalsNRM$agri.filter <- NULL
+#capitalsNRM$agri.filter <- NULL
 
 # invert deer density
 invert <- capitalsNRM$deer.density - 1
@@ -131,4 +135,4 @@ ggplot(capitalsNRM_long)+
   scale_fill_viridis()+
   theme_bw()
 
-write.csv(capitalsNRM, paste0(dirData,"/output/capitals_normalised_Feb21.csv"),row.names = F)
+write.csv(capitalsNRM, paste0(dirData,"/output/capitals_normalised_Aug21.csv"),row.names = F)
